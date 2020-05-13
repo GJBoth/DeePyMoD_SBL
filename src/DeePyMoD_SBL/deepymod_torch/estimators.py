293b3f43c9b02404c043d429d757ae68c2cff9c3
Gert-Jan <gertjanboth@gmail.com>
2020-05-13 15:00:48 +0200

    Updated estimators.

--- src/DeePyMoD_SBL/deepymod_torch/estimators.py
+++ src/DeePyMoD_SBL/deepymod_torch/estimators.py
@@ -3,80 +3,71 @@ We keep the API in line with scikit learn (mostly), so scikit learn can also be 
 See scikitlearn.linear_models for applicable estimators.'''
 
 import numpy as np
-import torch
 from sklearn.cluster import KMeans
-from sklearn.linear_model import LassoLarsIC
 from pysindy.optimizers import STLSQ
-from sklearn.model_selection import train_test_split
+from sklearn.linear_models import LassoCV
 
 import warnings
-warnings.filterwarnings("ignore", category=UserWarning) # To silence annoying pde find warnings
+warnings.filterwarnings("ignore", category=UserWarning)  # To silence annoying pysindy warnings
 
 
-class Threshold():
-    '''Threshold based sparsity estimator'''
-    def __init__(self, threshold):
+class Threshold:
+    '''Performs additional thresholding on coefficient result from estimator. Basically
+    a thin wrapper around the given estimator. '''
+    def __init__(self, threshold=0.1, estimator=LassoCV):
+        self.estimator = estimator
         self.threshold = threshold
-    
+
+        # Library contains offset so turn off the intercept
+        self.estimator.set_params(fit_intercept=False)
+
     def fit(self, X, y):
-        coeff = np.linalg.lstsq(X, y[:, None], rcond=None)[0].squeeze() # SK learn estimators have 1D in and output
-        coeff[np.abs(coeff) < self.threshold]  = 0.0
-        
-        self.coef_ = coeff # to keep in line width sklearn
+        coeff = self.estimator.fit(X, y).coef_
+        coeff[np.abs(coeff) < self.threshold] = 0.0
+
+        self.coef_ = coeff  # to keep in line width sklearn
         return self
 
-    
+
 class Clustering():
     ''' Sparsity estimator based on k-means clusternig.'''
-    def __init__(self):
+    def __init__(self, estimator=LassoCV):
+        self.estimator = estimator
         self.kmeans = KMeans(n_clusters=2)
-        
+
+        # Library contains offset so turn off the intercept
+        self.estimator.set_params(fit_intercept=False)
+
     def fit(self, X, y):
-        coeff = np.linalg.lstsq(X, y[:, None], rcond=None)[0] # SK learn estimators have 1D in and output
+        coeff = self.estimator.fit(X, y).coef_
         clusters = self.kmeans.fit_predict(np.abs(coeff)).astype(np.bool)
-        
+
+        # make sure terms to keep are 1 and to remove are 0
         max_idx = np.argmax(np.abs(coeff))
-        if clusters[max_idx] != 1:  #make sure terms to keep are 1 and to throw are 0
+        if clusters[max_idx] != 1:
             clusters = ~clusters
-            
-        self.coef_ = clusters.astype(np.float32) #to keep in line with sklearn
+
+        self.coef_ = clusters.astype(np.float32)  #to keep in line with sklearn
         return self
 
-
-class ClusteringL1():
-    ''' Sparsity estimator based on k-means clusternig.'''
-    def __init__(self, lasso):
-        self.lasso = lasso
-        self.kmeans = KMeans(n_clusters=2)
-        
-    def fit(self, X, y):
-        coeff = self.lasso.fit(X, y).coef_
-        clusters = self.kmeans.fit_predict(np.abs(coeff[:, None])).astype(np.bool)
-        
-        max_idx = np.argmax(np.abs(coeff))
-        if clusters[max_idx] != 1:  #make sure terms to keep are 1 and to throw are 0
-            clusters = ~clusters
-            
-        self.coef_ = clusters.astype(np.float32) #to keep in line with sklearn
-        return self
 
 class PDEFIND():
     def __init__(self, lam=1e-5, dtol=1, **kwargs):
         self.lam = lam
         self.dtol = dtol
         self.kwargs = kwargs
-        
+
     def fit(self, X, y):
         coeff = PDEFIND.TrainSTRidge(X, y[:, None], self.lam, self.dtol, **self.kwargs)
         self.coef_ = coeff.squeeze()
         return self
-    
+
     @staticmethod
     def TrainSTRidge(R, Ut, lam, d_tol, maxit = 25, STR_iters = 10, l0_penalty = None, normalize = 2, split = 0.8, print_best_tol = False):
         """
         This function trains a predictor using STRidge.
 
-        It runs over different values of tolerance and trains predictors on a training set, then evaluates them 
+        It runs over different values of tolerance and trains predictors on a training set, then evaluates them
         using a loss function on a holdout set.
 
         Please note published article has typo.  Loss function used here for model selection evaluates fidelity using 2-norm,
@@ -92,7 +83,7 @@ class PDEFIND():
         TestR = R[test,:]
         TrainY = Ut[train,:]
         TestY = Ut[test,:]
-        D = TrainR.shape[1]       
+        D = TrainR.shape[1]
 
         # Set up the initial tolerance and l0 penalty
         d_tol = float(d_tol)
@@ -111,9 +102,9 @@ class PDEFIND():
             # Get a set of coefficients and error
             opt = STLSQ(threshold=tol, alpha=lam, fit_intercept=False)
             w = opt.fit(R, Ut).coef_.T
-            err = np.linalg.norm(TestY - TestR.dot(w), 2) + l0_penalty*np.count_nonzero(w)
+            err = np.linalg.norm(TestY - TestR.dot(w), 2) + l0_penalty * np.count_nonzero(w)
             # Has the accuracy improved?
-            if (err <= err_best) and (not np.all(w ==0)):
+            if (err <= err_best) and (not np.all(w == 0)):
                 err_best = err
                 w_best = w
                 tol_best = tol
@@ -124,4 +115,3 @@ class PDEFIND():
                 d_tol  = 2*d_tol / (maxit - iter)
                 tol = tol + d_tol
         return w_best
-    
