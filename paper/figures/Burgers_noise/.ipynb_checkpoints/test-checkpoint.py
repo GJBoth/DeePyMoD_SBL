b4b90080f4f9fea8fdf4426e0708e807b193242f
Gert-Jan <gert-jan.both@cri-paris.org>
2020-05-28 11:29:43 +0000

    Last chnages to algo + paper stuff

--- paper/figures/Burgers_noise/.ipynb_checkpoints/test-checkpoint.py
+++ paper/figures/Burgers_noise/.ipynb_checkpoints/test-checkpoint.py
@@ -8,8 +8,8 @@ from phimal_utilities.data.burgers import BurgersDelta
 from DeePyMoD_SBL.deepymod_torch.library_functions import library_1D_in
 from DeePyMoD_SBL.deepymod_torch.DeepMod import DeepModDynamic
 from DeePyMoD_SBL.deepymod_torch.training import train_dynamic, train_dynamic_old
-from DeePyMoD_SBL.deepymod_torch.estimators import Clustering
-from sklearn.linear_model import LassoLarsIC
+from DeePyMoD_SBL.deepymod_torch.estimators import Clustering, Threshold
+from sklearn.linear_model import LassoLarsIC, LassoCV
 
 if torch.cuda.is_available():
     torch.set_default_tensor_type('torch.cuda.FloatTensor')
@@ -28,14 +28,15 @@ x_grid, t_grid = np.meshgrid(x, t, indexing='ij')
 dataset = Dataset(BurgersDelta, v=v, A=A)
 
 noise_range = np.arange(0.0, 1.01, 0.05)#np.arange(0.0, 0.51, 0.05)
-n_runs = 2
+n_runs = 1
 
-for noise_level in [0.8]:
+for noise_level in [0.3]:
     for run in np.arange(n_runs):
-        X_train, y_train = dataset.create_dataset(x_grid.reshape(-1, 1), t_grid.reshape(-1, 1), n_samples=1000, noise=noise_level, random=True, return_idx=False, random_state=42) # use the same dataset for every run; only diff is in the network
-        estimator = Clustering(estimator=LassoLarsIC(fit_intercept=False))
+        X_train, y_train = dataset.create_dataset(x_grid.reshape(-1, 1), t_grid.reshape(-1, 1), n_samples=1000, noise=noise_level, random=True, return_idx=False, random_state=run) # use the same dataset for every run; only diff is in the network
+        #estimator = Clustering(estimator=LassoLarsIC(fit_intercept=False))
+        estimator = Threshold(0.1, LassoCV(cv=5, fit_intercept=False))
         config = {'n_in': 2, 'hidden_dims': [30, 30, 30, 30, 30], 'n_out': 1, 'library_function':library_1D_in, 'library_args':{'poly_order':2, 'diff_order': 3}, 'sparsity_estimator': estimator}
         model = DeepModDynamic(**config)
-        optimizer = torch.optim.Adam(model.parameters(), betas=(0.99, 0.99), amsgrad=True)
-        train_dynamic(model, X_train, y_train, optimizer, 6000, log_dir=f'testruns/new_stop_cluster_run_{run}/')
+        optimizer = torch.optim.Adam(model.parameters(), betas=(0.99, 0.999), amsgrad=True)
+        train_dynamic(model, X_train, y_train, optimizer, 6000, log_dir=f'testruns/old_hyperparams_low_noise/')
 
---
+++ paper/figures/Diffusion_2D/parsing.py
@@ -0,0 +1,47 @@
+import numpy as np
+from phimal_utilities.analysis import load_tensorboard
+import pandas as pd
+from os import listdir
+
+def collect_runs(ID):
+    files = [file for file in listdir(f'{ID}/runs/') if file[:len(ID)] == ID]#getting and sorting files
+    files.sort()
+
+    df_plot = pd.DataFrame() #df used to store results in
+    noise = []
+    run = []
+    coeffs = []
+    ini_coeffs = []
+    ini_idx = []
+    for file in files:
+        df = load_tensorboard(f'{ID}/runs/{file}/')
+        scaled_coeff_keys = [key for key in df.keys() if key[:6]=='scaled']
+        coeffs.append(df.tail(1))
+        noise.append(float(file.split('_')[1]))
+        run.append(int(file.split('_')[3]))
+        ini_sparse_idx = np.any(df[scaled_coeff_keys] == 0, axis=1).idxmax() - 1 # we want the step before
+        ini_idx.append(ini_sparse_idx)
+        #ini_coeffs.append(df[scaled_coeff_keys].iloc[ini_sparse_idx].to_numpy())
+        print(df.shape, ini_sparse_idx)
+        print(f'Done with {file}')
+        
+        
+    df_plot['noise'] = noise
+    df_plot['run'] = run
+    #df_plot['first_sparsity'] = ini_idx
+    df_coeffs = pd.concat(coeffs).reset_index(drop=True)
+    #df_ini_coeffs = pd.DataFrame(np.stack(ini_coeffs, axis=0), columns = ['ini_' + key for key in scaled_coeff_keys])
+    #df_plot = pd.concat([df_plot, df_coeffs, df_ini_coeffs], axis=1)
+    df_plot = pd.concat([df_plot, df_coeffs], axis=1)
+    
+    return df_plot
+
+# Now run for all 
+#df = collect_runs('threshold')
+#df.to_pickle('data/threshold_collected.pd')
+
+#df = collect_runs('cluster')
+#df.to_pickle('data/cluster_collected.pd')
+
+df = collect_runs('pdefind')
+df.to_pickle('data/pdefind_collected.pd')
